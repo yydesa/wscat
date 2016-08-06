bf9990fefca7fdae3c18ee0004fdfd0a3fad242e
Derek Sarnay <yyx@gmx.de>
2016-08-06 23:06:33 +0200

    wscat: add --suffix

--- wscat.go
+++ wscat.go
@@ -17,6 +17,7 @@ import (
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
+var suffix = flag.String("suffix", "", "url suffix")
 
 func main() {
 	flag.Parse()
@@ -25,7 +26,7 @@ func main() {
 	//interrupt := make(chan os.Signal, 1)
 	//signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/webssh"+*suffix}
 	log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
