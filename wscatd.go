aee7be4832ece049c2bb66a22623bed245d8b907
Derek Sarnay <yyx@gmx.de>
2020-01-03 10:54:07 +0100

    disable origin checking
    So far this was for ssh only anyway, and there is no browser
    that could send origin headers (that don't match the hostname
    the server assumes behind a reverse proxy).

--- wscatd.go
+++ wscatd.go
@@ -16,7 +16,9 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var docroot = flag.String("path", ".", "http root directory")
 
-var upgrader = websocket.Upgrader{} // use default options
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func (r *http.Request) bool { return true; },
+}
 
 type errmsg struct {
 	where string;
